ca7cd33fa9db8080acf364e5a8203ea93dcaded1
nicholas hays <nicholas.hays@hotmail.com>
2016-05-01 14:06:10 -0700

    modified nav component

--- app/nav/nav.component.js
+++ app/nav/nav.component.js
@@ -29,7 +29,7 @@ System.register(['angular2/core', 'angular2/router', 'angular2/http', '../home/h
         execute: function() {
             NavComponent = (function () {
                 function NavComponent() {
-                    this.tooltipText = "Team Members: Ethan Rowell,\n    Edward Koval";
+                    this.tooltipText = 'Team Members: Ethan Rowell,';
                 }
                 NavComponent.prototype.ngOnInit = function () {
                     $('.easterEgg').attr({
