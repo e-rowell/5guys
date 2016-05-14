f5278833c6b67c42dcd5bc5e37c448112ce7f485
something <nnebp@users.noreply.github.com>
2016-05-13 18:04:04 -0700

    fix server.js

--- server.js
+++ server.js
@@ -7,6 +7,5 @@ app.listen(3000, () => {
 })
 
 app.get('/', (req, res) => {
-    //res.sendFile(__dirname + '/index.html');
-    app.get('/', routes.index);
+    res.sendFile(__dirname + '/index.html');
 })
