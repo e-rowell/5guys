6377b6dde6415efece8bca579ef73935f46c17c5
Ethan Rowell <e-rowell@users.noreply.github.com>
2016-05-18 01:09:01 -0700

    Update registration.component.ts

--- app/components/login/registration.component.ts
+++ app/components/login/registration.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {Component} from 'angular2/core'
 @Component({
     selector: 'login',
@@ -13,28 +12,3 @@ export /**
     constructor() {
 
     }
-    
-    
-    
-=======
-//author: Nicholas Hays and Ethan Rowell
-
-import {Component} from 'angular2/core'
-@Component({
-    selector: 'login',
-    template: 'app/login/login.component.html'
-})
-export /**
- * LoginComponent
- */
-    class LoginComponent {
-    userName: string;
-    password: string;
-    constructor() {
-
-    }
-    
-    
-    
->>>>>>> cddeead923fcbf906bdac0d83c7d50cfced45519
-}
