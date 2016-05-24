3b2eedd4920f039da6824b5b9b21844be95881da
Ethan Rowell <ethan.rowell11@gmail.com>
2016-05-24 16:46:20 -0700

    merge upstream server.js

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 var db;
-var storage = multer.diskStorage({
+var username = false;
+var storage =  multer.diskStorage({
     destination: function (req, file, callback) {
         callback(null, './uploads');
     },
-/*    filename: function (req, file, callback) {
-        callback(null, file.fieldname);
-    }*/
+    //filename: function (req, file, callback) {
+    //    callback(null, file.fieldname + '-' + Date.now());
+    //}
 });
 
-var upload = multer({storage: storage}).single('userEntry');
+var upload = multer({ storage : storage}).single('userEntry');
 
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/node_modules'));
@@ -46,39 +47,71 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-
-app.get('/success', function (req, res) {
-    res.sendFile(__dirname + '/success.html');
-});
-
-/*check if the username and password are in the db
- This is a shit way to do this. how do we know the user is
- "logged in" when they are on another page?*/
+/*Better way to login*/
 app.post('/login', function (req, res) {
-    var result = false;
 
-    db.collection('users').find().toArray(function (err, array) {
-        for (var i = 0; i < array.length; i++) {
-            if ((array[i].username == req.body.username)
-                && (array[i].password == req.body.password)) {
-                result = true;
-            }
+    db.collection('users').findOne( {"username": req.body.username, "password": req.body.password } ,function (err, result) {
+        if (result) { //what if user is already logged in TODO
+            username = req.body.username;
+            res.status(200).send("login succeded")
         }
-
-        if (result)
-            res.redirect('/success');
         else
-            res.redirect('/');
+            res.status(500).send("login failed");
     })
 });
 
-var eventsJson = require("./api/events/events.json");
+app.get('/whoami', (req, res) => {
+    res.send(username);
+});
+
+app.get('/logout', (req, res) => {
+    //TODO error when not logged in?
+    username = false;
+    res.status(200).send("logged out");
+});
+
+app.post('/getEntry', (req, res) => {
+    //TODO why the fuck is this an array
+    db.collection('entries').find( {"username": {$eq: req.body.username}, "eventName": {$eq: req.body.eventName} } ).toArray((err, result) => {
+        if (err) return console.log(err);
+      res.send(result);
+    })
+});
+
+app.post('/getAllEntries', (req, res) => {
+    db.collection('entries').find( {"eventName": req.body.eventName}, (err, result) => {
+        res.send(result);
+    })
+});
+
+app.post('/submitEntry', (req, res) => {
+    upload(req,res,function(err) {
+        if(err) {
+                return res.end("Error uploading file.");
+        }
+
+        db.collection('entries').save(req.file, (err, result) => {
+            if (err) return console.log(err)
+
+        })
+            //TODO do something reasonable 
+            res.end("File is uploaded");
+    });
+});
+
+app.post('/createEvent', (req, res) => {
+    db.collection('events').save(req.body, function (err, result) {
+        if (err) return console.log(err)
+
+        console.log('New event created');
+        res.redirect('/');//TODO get rid of this?
+    })
+});
 
 app.get('/getAllEvents', (req, res) => {
     db.collection('events').find().toArray((err, result) => {
         if (err) return console.log(err);
-
-        res.json(eventsJson);
+      res.send(result);
     })
 });
 
@@ -101,7 +134,6 @@ app.post('/register', function (req, res) {
         if (err) return console.log(err)
 
         console.log('New user registered');
-        res.redirect('/');
     })
 });
 
