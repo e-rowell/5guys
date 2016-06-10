37c747be31a3edd44c36ac01e4e16a48c900c75d
Edward Koval <ekoval@uw.edu>
2016-06-09 21:50:25 -0700

    added javadoc to tests

--- tests/event.spec.js
+++ tests/event.spec.js
@@ -5,6 +5,10 @@ System.register([], function(exports_1, context_1) {
     return {
         setters:[],
         execute: function() {
+            /*
+             a series of unit tests that confirm the events object is instantiated.
+             @author Edward Koval
+             */
             describe('make event', () => {
                 let app;
                 let testEvent;
@@ -23,43 +27,95 @@ System.register([], function(exports_1, context_1) {
                     eventEnd: 'end',
                     eventRequirements: ['to have requirements', 'akffjk'],
                     eventStatus: 'true' };
+                /*
+                 tests to make sure the event is not null.
+                 @author Edward Koval
+                 */
                 it('make sure event is not null', () => {
                     expect(testEvent).not.toBe(undefined);
                 });
+                /*
+                 tests to make sure the eventId is correct.
+                 @author Edward Koval
+                 */
                 it('test eventId', () => {
                     expect(testEvent.eventID).toBe(2);
                 });
+                /*
+                 tests to make sure the eventName is correct.
+                 @author Edward Koval
+                 */
                 it('testing eventName', () => {
                     expect(testEvent.eventName).toBe("testEvent");
                 });
+                /*
+                 tests to make sure the eventDescription is correct.
+                 @author Edward Koval
+                 */
                 it('testing eventDescription', () => {
                     expect(testEvent.eventDescription).toBe("test");
                 });
+                /*
+                 tests to make sure the scoringGuidelines is correct.
+                 @author Edward Koval
+                 */
                 it('testing scoringGuidelines', () => {
                     expect(testEvent.scoringGuidelines).toBe("scoring");
                 });
+                /*
+                 tests to make sure the eventStart is correct.
+                 @author Edward Koval
+                 */
                 it('testing eventStart', () => {
                     expect(testEvent.eventStart).toBe("start");
                 });
+                /*
+                 tests to make sure the eventEnd is correct.
+                 @author Edward Koval
+                 */
                 it('testing eventEnd', () => {
                     expect(testEvent.eventEnd).toBe("end");
                 });
+                /*
+                 tests to make sure the eventRequirements is the correct length.
+                 @author Edward Koval
+                 */
                 it('testing eventRequirements length', () => {
                     expect(testEvent.eventRequirements.length).toBe(2);
                 });
+                /*
+                 tests to make sure the eventRequirements is correct.
+                 @author Edward Koval
+                 */
                 it('testing eventRequirements', () => {
                     expect(testEvent.eventRequirements[0]).toBe('to have requirements');
                 });
+                /*
+                 tests to make sure the eventStatus is correct.
+                 @author Edward Koval
+                 */
                 it('testing eventStatus', () => {
                     expect(testEvent.eventStatus).toBe("true");
                 });
+                /*
+                 tests to make sure the event service is created.
+                 @author Edward Koval
+                 */
                 describe('make service', () => {
                     // var EventsService = require('../app/services/event.service');
                     //
                     // var eventService = new EventsService();
+                    /*
+                     tests to make sure the event service is not instantiated.
+                     @author Edward Koval
+                     */
                     it('make service object', () => {
                         expect(service).toBe(undefined);
                     });
+                    /*
+                     tests to make sure the event service is not null.
+                     @author Edward Koval
+                     */
                     it('service null check', () => {
                         expect(service).not.toBe(null);
                     });
--- tests/event.spec.ts
+++ tests/event.spec.ts
@@ -16,6 +16,12 @@ import {AppComponent} from '../app/components/app/app.component';
 import {Observable} from "rxjs/Rx";
 import {InjectableMetadata} from "angular2/core";
 
+
+/*
+ a series of unit tests that confirm the events object is instantiated.
+ @author Edward Koval
+ */
+
 describe('make event', () => {
     let app:AppComponent;
     let testEvent:IEvent;
@@ -38,56 +44,108 @@ describe('make event', () => {
     eventRequirements: ['to have requirements', 'akffjk'],
     eventStatus: 'true'}
 
+    /*
+     tests to make sure the event is not null.
+     @author Edward Koval
+     */
     it('make sure event is not null', () => {
         expect(testEvent).not.toBe(undefined);
     });
 
+    /*
+     tests to make sure the eventId is correct.
+     @author Edward Koval
+     */
     it('test eventId', () => {
         expect(testEvent.eventID).toBe(2);
     })
 
+    /*
+     tests to make sure the eventName is correct.
+     @author Edward Koval
+     */
     it('testing eventName', () => {
         expect(testEvent.eventName).toBe("testEvent");
     })
 
+    /*
+     tests to make sure the eventDescription is correct.
+     @author Edward Koval
+     */
     it('testing eventDescription', () => {
         expect(testEvent.eventDescription).toBe("test");
     })
 
+    /*
+     tests to make sure the scoringGuidelines is correct.
+     @author Edward Koval
+     */
     it('testing scoringGuidelines', () => {
         expect(testEvent.scoringGuidelines).toBe("scoring");
     })
 
+    /*
+     tests to make sure the eventStart is correct.
+     @author Edward Koval
+     */
     it('testing eventStart', () => {
         expect(testEvent.eventStart).toBe("start");
     })
 
+    /*
+     tests to make sure the eventEnd is correct.
+     @author Edward Koval
+     */
     it('testing eventEnd', () => {
         expect(testEvent.eventEnd).toBe("end");
     })
 
+    /*
+     tests to make sure the eventRequirements is the correct length.
+     @author Edward Koval
+     */
     it('testing eventRequirements length', () => {
         expect(testEvent.eventRequirements.length).toBe(2);
     })
 
+    /*
+     tests to make sure the eventRequirements is correct.
+     @author Edward Koval
+     */
     it('testing eventRequirements', () => {
         expect(testEvent.eventRequirements[0]).toBe('to have requirements');
     })
 
+    /*
+     tests to make sure the eventStatus is correct.
+     @author Edward Koval
+     */
     it('testing eventStatus', () => {
         expect(testEvent.eventStatus).toBe("true");
     })
 
+    /*
+     tests to make sure the event service is created.
+     @author Edward Koval
+     */
     describe('make service', () => {
 
         // var EventsService = require('../app/services/event.service');
         //
         // var eventService = new EventsService();
 
+        /*
+         tests to make sure the event service is not instantiated.
+         @author Edward Koval
+         */
         it('make service object', () => {
             expect(service).toBe(undefined)
         })
 
+        /*
+         tests to make sure the event service is not null.
+         @author Edward Koval
+         */
         it('service null check', () => {
             expect(service).not.toBe(null)
         })
